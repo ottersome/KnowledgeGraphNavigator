c6bb3019f9f427473898b42c565b6f78ab76b62c
Luis Garcia <luisgarcia26284@gmail.com>
2024-04-10 23:24:14 +0800

    Adding: Main and Model

--- main.py
+++ main.py
@@ -1,6 +1,40 @@
+# %% Important imports
+import argparse
 import os
 
-# Load data
+from transformers import AutoTokenizer
+
+from kgraphs.data.data import BasicDataset, DatasetFactory
+from kgraphs.utils.logging import create_logger
+
+# %% Some global initalization
+logger = create_logger("MAIN")
 
 
-# Load Tokenizer
+def argsies():
+    ap = argparse.ArgumentParser()
+    ap.add_argument("--dataset_name", default="manu/project_gutenberg")
+    ap.add_argument("--tokenizer_name", default="facebook/bart-base")
+    ap.add_argument("--model_tokenwindow_size", default=1024)
+    ap.add_argument("--token_count_cap", default=1000000)
+
+    return ap.parse_args()
+
+
+# %% Main Functions
+if __name__ == "__main__":
+    args = argsies()
+
+    # Load the Tokenizer
+    tokenizer = AutoTokenizer.from_pretrained(args.tokenizer_name)
+
+    dataset = DatasetFactory(
+        dataset_name=args.dataset_name,
+        ptrnd_tknzr=tokenizer,
+        window_size=args.model_tokenwindow_size,
+        amnt_tkns_for_training=args.token_count_cap,
+    )
+
+    dataset.load_split()
+
+    # Once the dataset is build we can load the model and train it on the stream
