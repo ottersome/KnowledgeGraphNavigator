2fdf84bf2e2d148bb9235168d3b2e4fb0ec18227
Luis Garcia <luisgarcia26284@gmail.com>
2024-06-28 02:57:27 +0800

    Adding: lightning fit

--- standard_transformer.py
+++ standard_transformer.py
@@ -43,6 +43,7 @@ def argsies():
     ap.add_argument("--dropout_rate", default=0.1)
     ap.add_argument("--masking_percentage", default=0.1)
     ap.add_argument("--raw_ds_location", default="./data/raw/")
+    ap.add_argument("--seed", default=42,type=int)
 
     ap.add_argument("--chkpnt_loc", default="./checkpoints", type=str)
 
@@ -52,6 +53,13 @@ def argsies():
     ap.add_argument("--wr_notes", help="Wand Run Notes", type=str)
 
     return ap.parse_args()
+
+def set_all_seeds(seed):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
 
 
 class TextDataSet(Dataset):
@@ -73,6 +81,7 @@ if __name__ == "__main__":
     # Initialize wandb
     if args.wandb:
         wandb.init(project="kgraphs")
+    set_all_seeds(args.seed)
 
     # Load the Tokenizer
     tokenizer: BartTokenizer = AutoTokenizer.from_pretrained(args.model_name)
@@ -99,7 +108,7 @@ if __name__ == "__main__":
     train_dl = DataLoader(
         train_ds,
         batch_size=args.batch_size,
-        shuffle=True,
+        # shuffle=True,
         drop_last=True,
         num_workers=1,
     )
@@ -165,107 +174,7 @@ if __name__ == "__main__":
     # )
 
     logger.info("Starting to train the model")
-    trainer.fit(lightning_module, train_dl, val_dl)
+    trainer.fit(lightning_module, train_dl, val_dl,ckpt_path="./checkpoints/epoch=0-step=2330.ckpt")
 
     exit()
 
-    ### Old Stuff
-    end_time = time()
-    time, unit = time_to_largest_unit(end_time - start_time)
-    logger.info(f"Loading data took {time:.2f} {unit} to run.")
-
-    logger.info(f"Using device {device}")
-
-    # Once the dataset is build we can load the model and train it on the stream
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
-    criterium = torch.nn.CrossEntropyLoss()
-
-    num_batches = floor(len(train_ds) / args.batch_size)
-    losses_epoch = []
-    # Inner and outer bars
-    e_bar = tqdm(range(args.epochs), desc="Epochs")
-    b_bar = tqdm(range(num_batches), desc="Batches")
-    for e in range(args.epochs):
-        # Extract the batch
-        losses_batch = []
-
-        b_bar.reset()
-        for b in range(num_batches):
-            # Get the batch
-            batch = train_ds.iloc[
-                b * args.batch_size : (b + 1) * args.batch_size
-            ].values.tolist()
-
-            # Dont deal with small batches
-            if len(batch) < args.batch_size:
-                continue
-            # Send data
-            target = torch.Tensor(batch).to(torch.long).to(device)
-            token_list_tensor = torch.tensor(batch).to(device)
-            mlmd_tensor = mask_tensor(  # type: ignore
-                token_list_tensor, tokenizer, float(args.masking_percentage)
-            ).to(torch.long)
-            # Multiply source by mask
-
-            result = model(mlmd_tensor, target)
-
-            loss = criterium(
-                result.view(-1, result.shape[-1]),
-                target.view(-1),
-            ).mean()
-            losses_batch.append(loss.item())
-            if len(losses_batch) > 5 and args.wandb:
-                wandb.log({"loss": sum(losses_batch[-5:]) / 5, "batch": b})
-
-            # Get the backpropagation details
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-            b_bar.update(1)
-
-            # TODO: perhaps save the model environment eventually
-        losses_epoch.append(sum(losses_batch) / len(losses_batch))
-        # Report locally and to wandb
-        logger.info(f"Epoch {e} has loss {losses_epoch[-1]}")
-        if args.wandb:
-            wandb.log({"loss": losses_epoch[-1], "epoch": e})
-
-        # Validate on a per epoch basis
-        e_bar.set_description("Evaluating")
-        model.eval()
-        num_eval_batches = len(val_ds) // args.batch_size
-        eval_losses = []
-        b_bar.set_description("Evaluation Batch")
-        for i in range(num_eval_batches):
-            batch = val_ds.iloc[
-                i * args.batch_size : (i + 1) * args.batch_size
-            ].values.tolist()
-
-            token_list_tensor = torch.Tensor(batch).to(torch.long).to(device)
-
-            mlmd_tensor = mask_tensor(  # type: ignore
-                token_list_tensor, tokenizer, float(args.masking_percentage)
-            ).to(torch.long)
-
-            result = model(mlmd_tensor, token_list_tensor)
-            softies = F.softmax(result, dim=-1)
-            chosen_ids = torch.argmax(softies, dim=-1)
-
-            # Log mlmd_tensor[:20] vs result[:20] vs token_list_tensor[:20] textually to see examples of guesses
-            corrupted_translated = tokenizer.batch_decode(mlmd_tensor[:, :20])
-            denosied_translated = tokenizer.batch_decode(chosen_ids[:, :20])
-            true_translated = tokenizer.batch_decode(token_list_tensor[:, :20])
-            logger.debug(f"MLMD: {corrupted_translated}")
-            logger.debug(f"Result: {denosied_translated}")
-            logger.debug(f"True: {true_translated}")
-
-            loss = criterium(
-                result.view(-1, result.shape[-1]),
-                token_list_tensor.view(-1),
-            )
-            eval_losses.append(loss.item())
-            b_bar.update(1)
-
-        # Present the validation
-
-        e_bar.update(1)
