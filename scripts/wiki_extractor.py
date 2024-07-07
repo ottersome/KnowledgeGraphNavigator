c3fe4e6cf7311f47cf011c419e43fedde89ac7fc
Luis Garcia <luisgarcia26284@gmail.com>
2024-07-07 23:58:23 +0800

    Adding: Extractor can seek through big dump.
    hehe

--- scripts/wiki_extractor.py
+++ scripts/wiki_extractor.py
@@ -1,6 +1,5 @@
 import bz2
 import os
-import pdb
 import pickle
 import random
 import signal
@@ -8,7 +7,10 @@ import sys
 import xml.etree.ElementTree as ET
 from argparse import ArgumentParser
 from time import sleep
-from typing import IO, Dict, List, Tuple
+from typing import IO, Dict, List, Sequence, Tuple
+
+import indexed_bzip2 as ibz2
+from tqdm import tqdm
 
 
 def argsies():
@@ -21,6 +23,11 @@ def argsies():
         default="/home/ottersome/Datasets/enwiki-20240620-pages-articles-multistream.xml.bz2",
         help="Path to your dump file.",
         type=str,
+    )
+    ap.add_argument(
+        "--block_offset_map_cache",
+        default="./.cache/bom_cache.dat",
+        help="Where to store block offset map",
     )
     ap.add_argument(
         "-o",
@@ -44,8 +51,9 @@ def argsies():
         action="store_true",
         help="Indent the output.",
     )
+    ap.add_argument("--read_chunk_size", default=1024, type=int)
     # Ask for mode, either extraction or sampling (multiple choice)
-    ap.add_argument("--mode", choices=["extract", "sample"], default="extract")
+    ap.add_argument("-m", "--mode", choices=["extract", "sample"], default="sample")
     # Make it so we ask for extract_output_dir only if we are in extract mode
     ap.add_argument("--cachedir", help="Where to store the cache", default="./.cache")
 
@@ -57,32 +65,153 @@ def argsies():
     return args
 
 
-def read_chunk(file_ptr: IO, read_chunk_size: int) -> Tuple[bytes, bool]:
+def create_page_index(
+    file_path: str, block_offset_map: Dict[int, int] = {}
+) -> Tuple[List[int], List[int]]:
+    global page_offsets, page_ids
+    current_file_offset = 0
+    page_ids = []
+    page_offsets = []
+    # Read amount of bytes without loading file into RAM
+    tot_numb_bytes = os.path.getsize(file_path)
+    print(f"Total number of bytes is {tot_numb_bytes/1e9}")
+
+    with ibz2.open(file_path, parallelization=os.cpu_count()) as wiki_dump_file:
+        wiki_dump_file.set_block_offsets(block_offset_map)
+        wiki_dump_file.seek(int(tot_numb_bytes * 0.98))
+        cur_page_id = -1
+        cur_page_title = ""
+        on_page = False
+
+        # For each line
+        cur_line = wiki_dump_file.readline()
+        items_added_so_far = 0
+        bar = tqdm(total=tot_numb_bytes)
+
+        while cur_line is not None:
+            cur_line = cur_line.decode("utf-8")
+            assert isinstance(
+                cur_line, str
+            ), f"Line is not a string, rather {type(cur_line)}"
+            stripped_line = cur_line.strip()
+            if stripped_line == "<page>":
+                on_page = True
+                page_offsets.append(current_file_offset)
+            elif stripped_line == "</page>":
+                # assert on_page, "We are not on a page"
+                if cur_page_id == -1:
+                    pass
+                    # page_offsets.pop()
+                else:
+                    items_added_so_far += 1
+                cur_page_id = -1
+                on_page = False
+            elif len(stripped_line) >= 8 and stripped_line[:7] == "<title>":
+                # Perhaps use later
+                cur_page_title = stripped_line[7:-8]
+            elif (
+                len(stripped_line) >= 4
+                and stripped_line[:4] == "<id>"
+                and cur_page_id == -1
+            ):
+                cur_page_id = int(stripped_line[4:-5])
+                page_ids.append(cur_page_id)
+            # current_file_offset += len(cur_line)
+            if items_added_so_far % 1000 == 0:
+                bar.set_description(f"Added {items_added_so_far} items")
+            bar.update(wiki_dump_file.tell() - current_file_offset)
+            current_file_offset = wiki_dump_file.tell()
+            cur_line = wiki_dump_file.readline()
+            if wiki_dump_file.tell() > tot_numb_bytes:
+                print(f"Cur line is :{cur_line}")
+    return page_offsets, page_ids
+
+
+def read_chunk_orderly(file_ptr: IO, read_chunk_size: int) -> Tuple[bytes, int]:
+    """
+    Unlike below, it assumes file_ptr points at the start of a chunk
+    """
+    # Assert that file pointe is 'rb'
+    assert file_ptr.mode == "rb", "File pointer is not in read binary mode"
+    assert has_data(file_ptr), "Nothing to read"
+    assert file_ptr.read(3) == b"BZh", "File pointer is not in read binary mode"
+
+    initial_pos = file_ptr.tell()
+    # Read first chunk length
+    # CHECK: We actually need to read 8 bytes
+    unit = int(1e5)
+    # Print read the single byte
+    chunk_length = int.from_bytes(file_ptr.read(1), byteorder="big") * unit
+    # Read thue chunk
+    print(f"Currently at pos {file_ptr.tell()} with chunk length {chunk_length}")
+    magic_number = file_ptr.read(3)
+    crc_checksum = file_ptr.read(3)
+    file_ptr.seek(0)
+    chunk = decompress_bzip2_block(file_ptr.read(chunk_length))
+    print(f"A bit of the chunk is {chunk[:100]}")
+
+    exit()
+    header_loc = file_ptr.tell()
+
+    file_ptr.seek(initial_pos)
+    return chunk, header_loc
+
+
+def decompress_bzip2_block(encoded_block):
+    # Decompress the bzip2 encoded block using the bz2 module
+    try:
+        decompressed_data = bz2.decompress(encoded_block)
+        return decompressed_data.decode(
+            "utf-8"
+        )  # Assuming the original data was UTF-8 encoded
+    except Exception as e:
+        print(f"Error during bzip2 decompression: {str(e)}")
+        return None
+
+
+def read_chunk(file_ptr: IO, read_chunk_size: int) -> Tuple[bytes, int]:
     """
     Read a chunk of bytes from a file
     """
     # Assert that file pointe is 'rb'
     assert file_ptr.mode == "rb", "File pointer is not in read binary mode"
-    assert peek(file_ptr) == b"BZ", "File pointer is not in read binary mode"
+    assert has_data(file_ptr), "Nothing to read"
 
-    header_loc = lambda bin_point: bin_point.find(b"BZh")
-    cur_chunk_offset = lambda chunk: len(chunk)
+    # Get final pos
+    og_pos = file_ptr.tell()
+    file_ptr.seek(0, 2)
+    final_pos = file_ptr.tell()
+    file_ptr.seek(0)
+    can_continue = lambda: final_pos > file_ptr.tell()
 
     chunk_so_far = file_ptr.read(read_chunk_size)
-    while header_loc(chunk_so_far[:-read_chunk_size]) == -1:
+    print(f"Read chunk of size {len(chunk_so_far)}")
+    header_loc = chunk_so_far.find(b"BZh")
+    print(f"HEader found at {header_loc}")
+    while header_loc == -1 and can_continue():
         next_chunk = file_ptr.read(read_chunk_size)
         if not next_chunk:
             break
         chunk_so_far += next_chunk
+        header_loc = chunk_so_far[-read_chunk_size:].find(b"BZh")
 
-    final_hdr_loc = header_loc(chunk_so_far[:-read_chunk_size])
-    if final_hdr_loc == -1:  # No more headers to be found
-        return chunk_so_far, False
+    # Back to og_position
+    file_ptr.seek(og_pos)
+    if header_loc == -1:  # No more headers to be found
+        return chunk_so_far, header_loc
     else:
-        return chunk_so_far[:-final_hdr_loc], True
+        return chunk_so_far[:-header_loc], header_loc
 
 
-def peek(file):
+def has_data(file: IO) -> bool:
+    current_position = file.tell()
+    file.seek(0, 2)
+    final_position = file.tell()
+    file.seek(current_position)
+    return final_position > current_position
+
+
+def peek(file, num_bytes=1):
     current_position = file.tell()
     data = file.read(num_bytes)
     file.seek(current_position)
@@ -90,13 +219,16 @@ def peek(file):
     return data
 
 
-def create_offsets_index(file_path: str) -> Tuple[List[int], List[int]]:
+def create_offsets_index(
+    file_path: str, block_offset_map: Dict[int, int] = {}
+) -> Tuple[List[int], List[int]]:
     global page_offsets, page_ids
     current_file_offset = 0
     page_ids = []
     page_offsets = []
 
-    with bz2.open(file_path, "rt") as wiki_dump_file:
+    with ibz2.open(file_path, parallelization=os.cpu_count()) as wiki_dump_file:
+        wiki_dump_file.set_block_offsets(block_offset_map)
         cur_page_id = -1
         cur_page_title = ""
         on_page = False
@@ -167,8 +299,29 @@ def keyboard_interrupt_handler(signal, frame):
 #     return decompressed_data[start : start + read_size]
 
 
+def create_block_offsets_map(bz2_path: str, cache_path: str) -> Dict[int, int]:
+    # Check if has already been created
+    if os.path.exists(cache_path):
+        print(f"Found cached offset map at {cache_path}. Loading... ")
+        with open(cache_path, "rb") as offsets_file:
+            return pickle.load(offsets_file)
+
+    file = ibz2.open(bz2_path, parallelization=os.cpu_count())
+    print(f"Creating block offset map for {bz2_path}")
+    block_offsets = file.block_offsets()  # can take a while
+    print(f"Block offset map created for {bz2_path}")
+    with open(cache_path, "wb") as offsets_file:
+        pickle.dump(block_offsets, offsets_file)
+    file.close()
+
+    return block_offsets
+
+
 def seek_and_read(
-    bz2_file_path, target_id, id_offset_dict: Dict[int, int], block_size=1024
+    bz2_file_path,
+    target_id,
+    id_offset_dict: Dict[int, int],
+    block_offset_map: Dict[int, int],
 ):
     """
     Given an id that can be found in page_ids we use the corresonding page_offsets to find the
@@ -177,21 +330,72 @@ def seek_and_read(
 
     # Find the nearest index entry
     offset = id_offset_dict[target_id]
-    with open(bz2_file_path, "rb") as f:
+    print(f"Id {target_id} has offset {offset}")
+    # with open(bz2_file_path, "rb") as f:
+    with ibz2.open(bz2_file_path, parallelization=os.cpu_count()) as f:
+        f.set_block_offsets(block_offset_map)
         f.seek(offset)
-        decompressor = bz2.BZ2Decompressor()
-        decompressed_data = b""
-        while len(decompressed_data) < read_size:
-            chunk = f.read(1024)
-            if not chunk:
-                break
-            pdb.set_trace()
-            decompressed_data += decompressor.decompress(chunk)
-    return decompressed_data
+        page_txt = retrieve_page(f)
+
+    return page_text
+
+
+def retrieve_page(
+    file_io: IO,
+) -> str:
+    # Ensure first line is <page>
+    assert file_io.readline().decode("utf-8") == "<page>"
+    # Look for content within <text> tags
+    while file_io.readline().decode("utf-8") != "</text>":
+        pass
+    content_ofinterst = ""
+    while file_io.readline().decode("utf-8") != "</page>":
+        content_ofinterst += file_io.readline().decode("utf-8")
+    return content_ofinterst
+
+
+def find_article(
+    file_path: str,
+    stream_offset: int,
+    article_offset: int,
+    article_title: str,
+):
+    next_offset = 700582
+    with open(file_path, "rb") as f:
+        # Seek to the start of the compressed stream
+        f.seek(stream_offset)
+        print(f"Reading {next_offset - stream_offset} bytes")
+        article_binary = f.read(next_offset - stream_offset)
+
+        # Decompress the stream from the current file position
+        print(f"Decompressing {len(article_binary)} bytes")
+        decompressed_data = bz2.decompress(article_binary)
+
+        # Convert bytes to string for processing
+        decompressed_data = decompressed_data.decode("utf-8")
+
+        print(f"Decompressed data is {decompressed_data}")
+
+        # Find the article within the decompressed data
+        start_index = decompressed_data.find(f"<title>{article_title}</title>")
+
+        if start_index == -1:
+            print(f"Article '{article_title}' not found.")
+            return None
+
+        # Extract the article content
+        end_index = decompressed_data.find("</page>", start_index) + len("</page>")
+        article_content = decompressed_data[start_index:end_index]
+
+        return article_content
 
 
 if __name__ == "__main__":
     args = argsies()
+
+    find_article(args.bz2_loc, 553, 247, "AbeceDarians")
+
+    exit()
 
     # # Lets first load and test the picle
     # with open(args.cachedir + "/index.pkl", "rb") as f:
@@ -204,12 +408,21 @@ if __name__ == "__main__":
         bz2_file_path = args.bz2_loc
         global index
         print(f"Using file {bz2_file_path}. This *will* take a while")
-        index = create_offsets_index(bz2_file_path)
+        # index = create_offsets_index(bz2_file_path)
+        # First Create the block offset map for future use
+        bom = create_block_offsets_map(bz2_file_path, args.block_offset_map_cache)
+        print("About to calculate index")
+        index = create_page_index(bz2_file_path, bom)
 
     elif args.mode == "sample":
-        print("Sampling from file")
         with open(args.cachedir + "/index.pkl", "rb") as f:
             id_offset_dict = pickle.load(f)
+            # Also look for block offset map
+            assert os.path.exists(
+                args.block_offset_map_cache
+            ), f"Block offset map not found at {args.block_offset_map_cache}"
+            bom = pickle.load(open(args.block_offset_map_cache, "rb"))
             sample_id = random.choice(list(id_offset_dict.keys()))
-            data = seek_and_read(args.bz2_loc, sample_id, id_offset_dict)
+            print(f"Sampling index {sample_id} from file")
+            data = seek_and_read(args.bz2_loc, sample_id, id_offset_dict, bom)
             print(data)
