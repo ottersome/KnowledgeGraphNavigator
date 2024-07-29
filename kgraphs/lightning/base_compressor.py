772018c1ee9c31c154e72b25487c74b347cd840f
Luis Garcia <luisgarcia26284@gmail.com>
2024-07-29 13:29:26 +0800

    Draft: Valdiation Step

--- kgraphs/lightning/base_compressor.py
+++ kgraphs/lightning/base_compressor.py
@@ -17,6 +17,7 @@ class BaseCompressor(L.LightningModule):
         tokenizer: BartTokenizer,
         masking_percentage: float = 0.15,
         lr: float = 0.0001,
+        # For debugging:
     ):
         super().__init__()
         self.model = compressor_model
@@ -29,7 +30,6 @@ class BaseCompressor(L.LightningModule):
         self.lr = lr
 
     def training_step(self, batch, batch_idx):
-        self.my_logger.info(f"Tryign to get somewhere within this")
         target = batch
         target = target.to(torch.long)
         target_flat = target.flatten()
@@ -44,8 +44,28 @@ class BaseCompressor(L.LightningModule):
         )
 
         loss = self.criterium(recovered_text_flat, target_flat)
+        self.my_logger.debug(f"Non avg loss shape is {loss.shape}")
+        loss_avg = loss.mean()
+        self.my_logger.debug(f"Loss is {loss_avg.item()}")
+        self.log("train_loss", loss_avg.item())
+        return loss_avg
 
-        return loss
+    def validation_step(self, batch, batch_idx):
+        self.my_logger.info(f"Performing validation for batch {batch_idx}")
+        target = batch
+        target = target.to(torch.long)
+        # target_flat = target.flatten()
+        recovered_text = self.model(target)
+        argmaxed_target = torch.argmax(target,dim=-1) 
+        detokenized = self.tokenizer.batch_decode(argmaxed_target)
+        # recovered_text_flat = recovered_text.view(-1, recovered_text.shape[2])
+
+        for b in range(recovered_text.shape[0]):
+            self.my_logger.debug(f"Detokenized looks like {detokenized}\n")
+            self.my_logger.debug(f"Target is {target[b]}")
+
+
+        self.log("val_loss", 0)
 
     def configure_optimizers(self):
         return torch.optim.Adam(self.model.parameters(), lr=self.lr)
